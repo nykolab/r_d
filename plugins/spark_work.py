dafc38060f5b4b98b46da216ff772796a29ea792
Mykola Bocharov <nykolab@gmail.com>
2021-05-26 22:24:44 +0300

    add spark sql

--- plugins/spark_work.py
+++ plugins/spark_work.py
@@ -31,12 +31,12 @@ spark = SparkSession.builder\
 film_category_df = spark.read.jdbc(pg_url, "film_category", properties=pg_properties)
 category_df      = spark.read.jdbc(pg_url, "category", properties=pg_properties)
 
-film_category_df = film_category_df.join(category_df
+film_category_df_join = film_category_df.join(category_df
                       , film_category_df.category_id == category_df.category_id
                       , 'left')\
                       .select(F.col('film_id'), F.col('name').alias('category_name'))
 
-num_films_in_category = film_category_df.groupBy(F.col('category_name'))\
+num_films_in_category = film_category_df_join.groupBy(F.col('category_name'))\
                             .agg(F.count(F.col('film_id')).alias('num_films'))\
                             .orderBy('num_films', ascending=False)
 
@@ -57,3 +57,20 @@ top_ten_actors = top_ten_actors_df.groupBy(F.col('actor_name'))\
                                             .agg(F.count(F.col('rental_id')).alias('num_rentals'))\
                                             .orderBy('num_rentals', ascending=False)\
                                             .limit(10)
+
+# 3 - вывести категорию фильмов, на которую потратили больше всего денег.
+
+payment_df = spark.read.jdbc(pg_url, "payment", properties=pg_properties)
+
+top_money_spent_category = payment_df.join(rental_df, payment_df.rental_id == rental_df.rental_id)\
+                                     .join(inventory_df, rental_df.inventory_id == inventory_df.inventory_id)\
+                                     .join(film_category_df, inventory_df.film_id == film_category_df.film_id)\
+                                     .join(category_df, film_category_df.category_id == category_df.category_id)\
+                                     .select(category_df.name.alias('category_name'), payment_df.amount.alias('money_spent'))
+
+top_money_spent_category = top_money_spent_category\
+                                .groupBy(F.col('category_name'))\
+                                .agg(F.count(F.col('money_spent'))\
+                                .alias('money_spent_on_category'))\
+                                .orderBy('money_spent_on_category', ascending=False)\
+                                .limit(1)
